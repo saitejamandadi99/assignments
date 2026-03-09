7b288d536ddfa6074c1b4c9ada7bffd569f6444b
saitejamandadi99 <saitejamandadi99>
2026-03-09 20:49:31 +0530

    boxing and unboxing program

---
+++ BoxingAndUnboxing/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BoxingAndUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10; //value type 
+            object obj = number; //boxing the value type into an reference type
+            Console.WriteLine($"boxed value: {obj}, {obj.GetType()}, {typeof(object)}"); // 
+
+            int newNumber = (int)obj; //unboxing the reference type back to value type
+            Console.WriteLine($"unboxed values: {newNumber}, {newNumber.GetType()}, {typeof(int)}");
+        
+        }
+    }
+}
