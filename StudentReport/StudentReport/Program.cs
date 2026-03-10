c1f6bc105773e91817c835cf2df6f47c08c4f8dd
saitejamandadi99 <saitejamandadi99>
2026-03-10 19:11:11 +0530

    added student report code

---
+++ StudentReport/StudentReport/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("ajith", 1, [60, 70, 98, 78, 66]);
+            Student student2 = new Student("Vicky", 2, [50, 90, 88, 98, 77]);
+            Student student3 = new Student("Arjun", 3, [99, 88, 100, 100, 99]);
+            Student student4 = new Student("Vijay", 4, [50, 70, 98, 38, 26]);
+            Console.WriteLine("Printing Student Details\n");
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.WriteLine(student3.ToString());
+            Console.WriteLine(student4.ToString());
+        }
+    }
+}
---
+++ StudentReport/StudentReport/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentReport
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public int RollNumber { get; set; }
+        public int[] MarksArray { get; set; }
+
+        public int AverageMarks { get; set; }
+
+        public string Grade { get; set; }
+
+
+
+        public Student(string name, int rollnumber , int[] marks)
+        {
+            StudentName = name;
+            RollNumber = rollnumber;
+            MarksArray = marks;
+
+            CalculateAverage();
+            CalculateGrade();
+        }
+
+        public override string ToString()
+        {
+            return $"Student: {StudentName}, Roll Number: {RollNumber}, Marks: {string.Join(", ",MarksArray)}, Average: {AverageMarks}, Grade: {Grade} ";
+        }
+
+        public void CalculateAverage()
+        {
+            int total = 0;
+            foreach (int mark in MarksArray)
+            {
+                total += mark;
+            }
+            AverageMarks =  total / MarksArray.Length;
+        }
+
+        public void CalculateGrade()
+        {
+            if (AverageMarks >= 90)
+            {
+                Grade = "A+";
+            }
+            else if (AverageMarks >= 80)
+            {
+                Grade = "A";
+            }
+            else if (AverageMarks >= 70)
+            {
+                Grade = "B";
+            }
+            else if (AverageMarks >= 60)
+            {
+                Grade = "C";
+            }
+
+            else if (AverageMarks >= 50)
+            {
+                Grade = "D";
+            }
+            else
+            {
+                Grade = "F";
+            }
+        }
+
+    }
+}
