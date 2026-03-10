ab728ef86864aebe5ce4a5f1711a7574b379da58
saitejamandadi99 <saitejamandadi99>
2026-03-10 17:32:34 +0530

    added planet enum

---
+++ PlanetEnumeration/PlanetEnumeration/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PlanetEnumeration
+{
+    internal class Program
+    {
+        enum Planet
+        {
+            Earth = 100,
+            Mars = 38,
+            Jupiter = 234
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your weight");
+            int.TryParse(Console.ReadLine(), out int weight);
+
+            foreach(Planet planet in Enum.GetValues(typeof(Planet)))
+            {
+                int gravity = (int)planet;
+                float planetgravity = gravity / 100f;
+                float PlanetWeight = planetgravity * weight;
+                Console.WriteLine($"Person of weight {weight}kg is {PlanetWeight}kg on planet {planet} ");
+            }
+        }
+    }
+}
