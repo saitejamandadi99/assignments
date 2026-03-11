0eecb3cd9e110f83ac9c0d7e2a11f0b48429196c
saitejamandadi99 <saitejamandadi99>
2026-03-11 16:07:07 +0530

    added in out params code

---
+++ Params/Params/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    internal class Methods
+    {
+        public  int Addition(out int count,params int[] numbers)
+        {
+             count = 0;
+            foreach (int number in numbers)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public  string Concatination(params string[] words)
+        {
+            string sentence = "";
+            foreach (string word in words)
+            {
+                sentence = sentence + word + " ";
+            }
+            return sentence;
+        }
+
+        public  int Ref_addition(int a)
+        {
+            return a += 1;
+        }
+
+        public  int Ref_subtraction(ref int b)
+        {
+            return b -= 5;
+        }
+
+
+    }
+}
---
+++ Params/Params/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Linq.Expressions;
+
+namespace Params
+{
+
+    
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int count;
+            Methods method = new Methods();
+            method.Addition(out count, 1, 2, 3, 4, 5, 6);
+            Console.WriteLine($"Count of the params is : {count}");
+            Console.WriteLine();
+            string sentence = method.Concatination("Good", "Morning");
+            Console.WriteLine(sentence);
+            int a = 10;
+            int b = 10;
+
+            Console.WriteLine($"original values: {a}");
+            Console.WriteLine($"original value {b}\n");
+
+
+            Console.WriteLine($"returned {method.Ref_addition(a)}");
+            Console.WriteLine($"returned {method.Ref_subtraction(ref b)}\n");
+
+            Console.WriteLine($"original values: {a}");
+            Console.WriteLine($"original value {b}");
+
+
+        }
+    }
+}
