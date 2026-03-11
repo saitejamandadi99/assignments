c0e4bc414f0f7f1c28abfec42670b63381999dae
saitejamandadi99 <saitejamandadi99>
2026-03-11 16:37:24 +0530

    added product app code

---
+++ Mar11/ProductApp/ProductApp/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ProductApp
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; } 
+        public double DiscountPercentage { get; set; }
+
+        public Product(int id , string name, double price, double discountPercentage)
+        {
+            Id = id;
+            Name = name;
+            Price = price; 
+            DiscountPercentage = discountPercentage;
+
+        }
+
+        public double GetDiscountPrice()
+        {
+            return Price - (Price * DiscountPercentage / 100f);
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Product Id: {Id}");
+            Console.WriteLine($"Product Name: {Name}");
+            Console.WriteLine($"Product Price: {Price}");
+            Console.WriteLine($"Product Discount Percentage: {DiscountPercentage}%");
+            Console.WriteLine($"Product Price after discount: {GetDiscountPrice()}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Mar11/ProductApp/ProductApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(1, "Apple Macbook", 65000, 20);
+            Product product2 = new Product(2, "Samsung Galaxy", 123000, 30);
+            product1.DisplayInfo();
+            product2.DisplayInfo();
+
+        }
+    }
+}
