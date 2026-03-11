ee5830996b63b18f31ad597dd91417886d14e3fe
saitejamandadi99 <saitejamandadi99>
2026-03-11 18:27:51 +0530

    added bank game app

---
+++ Mar11/Bank_Game_App/Bank_Game_App/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Game_App
+{
+    public class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public double Balance { get; set; }
+
+        public BankAccount(string accountHolder, double balance)
+        {
+            AccountHolder = accountHolder;
+            Balance = balance;
+        }
+
+        public double GetBalance() { return Balance; }
+
+        public void Deposit(double money) 
+        {
+            Balance += money;
+            Console.WriteLine($"Money deposited in {AccountHolder}'s account, Total Balance: {Balance}\n");
+        }
+
+        public void WithDraw(double amount)
+        { 
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient Balance\n");
+                return;
+            }
+            Balance -= amount;
+            Console.WriteLine($"Amount withdraw completed , Total Balance: {Balance}\n");
+
+        }
+
+    }
+
+}
---
+++ Mar11/Bank_Game_App/Bank_Game_App/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Bank_Game_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bank1 = new BankAccount("Ajay", 20000);
+            BankAccount bank2 = new BankAccount("Vijay", 22000);
+            Random random = new Random();
+
+            bank1.Deposit(random.Next(1000, 5000));
+            bank2.Deposit(random.Next(1000, 5000));
+
+            bank1.WithDraw(random.Next(1000, 5000));
+            bank2.Deposit(random.Next(1000, 5000));
+
+            bank1.Deposit(random.Next(1000, 5000));
+            bank2.WithDraw(random.Next(1000, 5000));
+
+            if(bank1.GetBalance() > bank2.GetBalance())
+            {
+                Console.WriteLine("Ajay is winner");
+            }
+            else if(bank1.GetBalance() < bank2.GetBalance())
+            {
+                Console.WriteLine("Vijay is winner");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie");
+            }
+
+        }
+    }
+}
