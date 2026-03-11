229ca66c30ef92fdcf40bea17644e3300ad8298f
saitejamandadi99 <saitejamandadi99>
2026-03-11 17:14:46 +0530

    added Book Assignment

---
+++ Mar11/Book_BestSeller/Book_BestSeller/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_BestSeller
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public bool IsBestSeller { get; set; }
+
+        public Book(string title, string author, double price, bool isBestSeller)
+        {
+            Title = title;
+            Author = author;
+            Price = price;
+            IsBestSeller = isBestSeller;
+        }
+
+        public double GetDiscountPrice()
+        {
+            double BookPrice = Price;
+            if (IsBestSeller)
+            {
+                BookPrice = BookPrice - BookPrice * 0.1; //BookPrice *= 0.90
+            }
+            if(BookPrice > 500) //even if it is not best seller discount will be added
+            {
+                BookPrice = BookPrice -  BookPrice * 0.05; //BookPrice *= 0.95
+            }
+            return BookPrice;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Original Price: {Price}");
+            //Console.WriteLine($"Best Seller? {IsBestSeller}");
+            Console.WriteLine($"Price after Discount: {GetDiscountPrice()}");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Mar11/Book_BestSeller/Book_BestSeller/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Book_BestSeller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Atomic Habits", "James Clear", 650, true);
+            Book book2 = new Book("Rich Dad, Poor Dad", "Robert Kiyosaki", 650, false);
+            book1.DisplayInfo();
+            book2.DisplayInfo();
+        }
+    }
+}
