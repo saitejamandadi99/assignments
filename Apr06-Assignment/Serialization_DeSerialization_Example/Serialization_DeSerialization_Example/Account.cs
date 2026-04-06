a41c4a397bba3fc9f7b9b67658d99572ff2c575d
saitejamandadi99 <saitejamandadi99>
2026-04-06 19:37:24 +0530

    added serialization assignment

---
+++ Apr06-Assignment/Serialization_DeSerialization_Example/Serialization_DeSerialization_Example/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_DeSerialization_Example
+{
+    public class Account
+    {
+        public int AccountNo { get; set; }
+        public string AccountHolderName { get; set; }
+        public string BankName { get; set; }
+        public double Balance { get; set; }
+
+        public Account() { }
+        public Account(int accNo, string name, string bankName, double balance)
+        {
+            AccountNo = accNo;
+            AccountHolderName = name;
+            BankName = bankName;
+            Balance = balance;
+        }
+
+        public void DepositAmount(double amount)
+        {
+            Balance += amount;
+            Console.WriteLine("Amount Deposited");
+            Console.WriteLine($"Amount {amount:C2} added to Account , Total Balance: {CheckBalance():C2}");
+        }
+
+        public void WithDrawl(double amount)
+        {
+            if(Balance - amount >= 500)
+            {
+                Balance -= amount;
+                Console.WriteLine("Amount Withdrawn");
+                Console.WriteLine($"Total Balance : {CheckBalance():C2}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance! Minimum Balance Required");
+                Console.WriteLine($"Total Balance : {CheckBalance():C2}");
+
+            }
+        }
+
+        public double CheckBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ Apr06-Assignment/Serialization_DeSerialization_Example/Serialization_DeSerialization_Example/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Text.Json;
+using System.Transactions;
+
+namespace Serialization_DeSerialization_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account accDetails = new Account();
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "accountDetails.json");
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                accDetails = JsonSerializer.Deserialize<Account>(json);
+                Console.WriteLine($"Welcome Back \nAccount Balance : {accDetails.CheckBalance():C2}");
+            }
+            else
+            {
+                
+                Console.WriteLine("Creating New Account : Required Details");
+                Console.WriteLine("Enter the Account No: ");
+                int.TryParse(Console.ReadLine(), out int accNo);
+                accDetails.AccountNo = accNo;
+                Console.WriteLine("Enter the Name of The Holder");
+                accDetails.AccountHolderName = Console.ReadLine();
+                Console.WriteLine("Enter the Name of the Bank");
+                accDetails.BankName = Console.ReadLine();
+                Console.WriteLine("Enter the Balance");
+                double balance;
+                while (true)
+                {
+                    double.TryParse(Console.ReadLine(), out double money);
+                    if(money > 500)
+                    {
+                        balance = money;
+                        break;
+                    }
+                    Console.WriteLine("Please enter amount above 500");
+
+                }
+                    accDetails.Balance = balance;
+
+                //serializing the data 
+                string data = JsonSerializer.Serialize(accDetails);
+                File.WriteAllText(filePath, data);
+            }
+
+            while (true)
+            {
+                Console.WriteLine("\n------Operations------\n");
+                Console.WriteLine("What do you wish to do\n1.Deposit\n2.WithDraw\n3.Display Balance\n4.Exit");
+                int.TryParse(Console.ReadLine(), out int option);
+                switch (option)
+                {
+
+                    case 1:
+                        Console.WriteLine("Enter the Amount to deposit");
+                        double.TryParse(Console.ReadLine(), out double amount);
+                        accDetails.DepositAmount(amount);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Amount to withdraw");
+                        double.TryParse(Console.ReadLine(), out double remove);
+                        accDetails.WithDrawl(remove);
+                        break;
+                    case 3:
+                        Console.WriteLine($"Total Balance in Account : {accDetails.AccountNo} - Balance: {accDetails.CheckBalance():C2}");
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you! Please Visit Again");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                string jsonData = JsonSerializer.Serialize(accDetails);
+                File.WriteAllText(filePath, jsonData);
+                Console.WriteLine();
+            }
+        }
+    }
+}
